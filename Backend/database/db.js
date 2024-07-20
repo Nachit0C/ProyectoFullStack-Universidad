86829e1986cc11a174bc915c91609a0f78d6acd0
Nachit0C <nachociccone@gmail.com>
2024-07-20 01:35:06 -0300

    sin variable de entorno

--- Backend/database/db.js
+++ Backend/database/db.js
@@ -2,15 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 // Configurar con los datos de la base de datos
 const coneccion = mysql.createConnection({
-    host: process.env.HOST,
+    /*host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    /*host:'mysql-urioplata.alwaysdata.net',
+    database: process.env.DATABASE,*/
+    host:'mysql-urioplata.alwaysdata.net',
     user:'urioplata',
     password:'Nachito1234#',
     database:'urioplata_db',
-    multipleStatements: true*/
+    multipleStatements: true
 });
 
 coneccion.connect( (err) => {

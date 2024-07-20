7bb888da7683734233ac50198a0462fd346b6843
Nachit0C <nachociccone@gmail.com>
2024-07-20 03:18:39 -0300

    a

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ myApp.use(myExpress.json());
 myApp.use(myExpress.static(path.join(__dirname, 'Frontend')));
 
 console.log('host:', process.env.HOST);
+console.log(process.env.PRUEBA);
 console.log(process.env.USER);
 console.log(process.env.PASSWORD);
 console.log(process.env.DATABASE);
